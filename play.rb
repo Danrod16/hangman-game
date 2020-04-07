0fef05bc35ef9fbb37a4364040de88096fa87122
Daniel Rodriguez <daniel.rod93@gmail.com>
2020-04-07 19:23:44 +0200

    Begin game working

--- play.rb
+++ play.rb
@@ -14,13 +14,35 @@ class Hangman
     ]
   end
 
-  def begin
-    # ask user for a letter
-    puts "A new game has started"
-    puts "Your clue is #{@word.last}"
-    puts "Enter a letter"
+  def make_guess
+    puts 'Enter a letter'
     guess = gets.chomp
 
+    #if letter is part of this word remove from letters array
+
+    good_guess = @word.first.include? guess
+
+    if good_guess
+      puts 'good guess!'
+    else
+      puts 'try again'
+    end
+  end
+
+  def begin
+    # ask user for a letter
+    puts "A new game has started.. you word is #{@word.first.size} characters long"
+    teaser
+    puts "Your clue is #{@word.last}"
+    make_guess
+  end
+
+  def teaser
+    word_teaser = ''
+    @word.first.size.times do
+      word_teaser += '_ '
+    end
+    puts word_teaser
   end
 end
 
