7d502eeb92d0bcc16cef64203df232a52c7c99ba
Daniel Rodriguez <daniel.rod93@gmail.com>
2020-04-09 18:17:05 +0200

    Hangman midwaay, to be continued

--- play.rb
+++ play.rb
@@ -2,6 +2,13 @@ class Hangman
   def initialize
     @letters = ('a'..'z')
     @word = words.sample
+    @lives = 7
+    @correct_guess = []
+    @word_teaser = ""
+
+    @word.first.size.times do
+      @word_teaser += '_ '
+    end
   end
 
   def words
@@ -14,20 +21,49 @@ class Hangman
     ]
   end
 
+  def teaser last_guess = nil
+    update_teaser(last_guess) unless last_guess.nil?
+    puts @word_teaser
+  end
+
+  def update_teaser
+    new_teaser = @word_teaser.split
+
+    new_teaser.each_with_index do |letter, index|
+    #replace blank values with letter that matches
+      if letter == '_ ' && @word.first[index] == last_guess
+        new_teaser[index] = last_guess
+      end
+    end
+    @word_teaser = new_teaser.join(' ')
+  end
+
   def make_guess
-    puts 'Enter a letter'
-    guess = gets.chomp
+    if @lives > 0
+      puts 'Enter a letter'
+      guess = gets.chomp
 
-    #if letter is part of this word remove from letters array
+      #if letter is part of this word remove from letters array
 
-    good_guess = @word.first.include? guess
+      good_guess = @word.first.include? guess
 
-    if good_guess
-      puts 'good guess!'
+      if good_guess
+        puts 'You are correct!'
+        @correct_guess << guess
+        #remove correct guess from letters
+        @letters.delete guess
+        teaser guess
+        make_guess
+      else
+        @lives -= 1
+        puts "Sorry you have #{@lives} left ... try again"
+        make_guess
+      end
     else
-      puts 'try again'
+      puts "game over"
     end
   end
+
 
   def begin
     # ask user for a letter
@@ -37,15 +73,7 @@ class Hangman
     make_guess
   end
 
-  def teaser
-    word_teaser = ''
-    @word.first.size.times do
-      word_teaser += '_ '
-    end
-    puts word_teaser
-  end
 end
 
 game = Hangman.new
 game.begin
-
